2939294a7f7d92541227a16649cb62001a1bb424
fujita <fujip8652@gmail.com>
2020-06-08 23:10:07 +0900

    眠い

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <style>
+  body {
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+  </style>
+</head>
+<body>
+Home
+</body>
+</html>
